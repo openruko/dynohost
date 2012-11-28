a68f562ad58f967b1f0d79604b362ea8c2958514
filirom1 <filirom1@gmail.com>
2012-11-28 15:32:22 +0000

    fix bad conf usage for logplex

--- dynohost/conf.js
+++ dynohost/conf.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   logplex: {
     hostname: env.LOGPLEX_HOST || 'localhost',
+    udpPort: env.LOGPLEX_UDP_PORT || 9996,
     webPort: env.LOGPLEX_WEB_PORT || 9999
   },
   rukorun: {
--- dynohost/logplex.js
+++ dynohost/logplex.js
@@ -2,15 +2,12 @@ var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
 var conf = require('./conf');
 
-var LOGPLEX_HOSTNAME = conf.logplex.hostname;
-var LOGPLEX_HOSTPORT = conf.logplex.port;
-
 function LogPlexClient(token) {
 
   this.write = function(msg) {
     var ts = +(new Date());
     var buffer = new Buffer(ts.toString() + ' ' + token + ' ' + msg);
-    client.send(buffer, 0, buffer.length, LOGPLEX_HOSTPORT, LOGPLEX_HOSTNAME);
+    client.send(buffer, 0, buffer.length, conf.logplex.udpPort, conf.logplex.hostname);
   };
 }
 
