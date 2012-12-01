b86eaebf95b6d89012b9054536529f9ab97b7268
filirom1 <filirom1@gmail.com>
2012-12-01 18:46:17 +0000

    switch logplex udp port and web port

--- dynohost/conf.js
+++ dynohost/conf.js
@@ -23,8 +23,8 @@ module.exports = {
   },
   logplex: {
     hostname: env.LOGPLEX_HOST || 'localhost',
-    udpPort: env.LOGPLEX_UDP_PORT || 9996,
-    webPort: env.LOGPLEX_WEB_PORT || 9999
+    webPort: env.LOGPLEX_WEB_PORT || 9996,
+    udpPort: env.LOGPLEX_UDP_PORT || 9999
   },
   rukorun: {
     path: env.RUKORUN_PATH
